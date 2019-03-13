de0aa3e8658c95b0da0b146413287f202cadcb10
Heiko Burkhardt <heiko@slock.it>
2019-03-13 10:53:07 +0100

    blank lines

--- contracts/libraries/SafeMath.sol
+++ contracts/libraries/SafeMath.sol
@@ -6,8 +6,8 @@ pragma solidity ^0.5.4;
  * @dev Unsigned math operations with safety checks that revert on error
  */
 
-
 library SafeMath {
+
     /**
      * @dev Multiplies two unsigned integers, reverts on overflow.
      */
