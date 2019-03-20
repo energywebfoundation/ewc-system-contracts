fb44836e1b88c32113ca6d2095ea4ab7743c29cc
ngyam <ngy.am.zs@gmail.com>
2019-03-20 10:22:43 +0100

    feat(validator-set): pragma solidity to 0.5.4

--- contracts/validatorset/ValidatorSetRelay.sol
+++ contracts/validatorset/ValidatorSetRelay.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.5.4;
 
 import "../misc/Ownable.sol";
 import "../interfaces/IValidatorSetRelay.sol";
--- contracts/validatorset/ValidatorSetRelayed.sol
+++ contracts/validatorset/ValidatorSetRelayed.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.5.4;
 
 import "../interfaces/IValidatorSetRelay.sol";
 import "../interfaces/IValidatorSetRelayed.sol";
