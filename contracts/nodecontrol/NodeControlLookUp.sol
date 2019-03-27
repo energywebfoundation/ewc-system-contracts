8cdae8b8a2ac3da8743ac4fb6362a9410596532f
ewf-updater <ewf-updater@ewf.org>
2019-03-27 14:04:51 +0100

    changed NodeControlLookup to be only one address for nodecontrol to look up

--- contracts/nodecontrol/NodeControlLookUp.sol
+++ contracts/nodecontrol/NodeControlLookUp.sol
@@ -4,8 +4,8 @@ pragma experimental ABIEncoderV2;
 
 ///@notice Serves as a lookup table. Currently used for the NodeControlContract
 contract NodeControlLookUp {
-    ///@notice 0 reserved for NodeControlLogic
-    mapping (uint => address) public list;
+    
+    address public nodeControlContract;
     address public owner;
 
     modifier onlyOwner {
@@ -18,9 +18,8 @@ contract NodeControlLookUp {
     }
 
     ///@notice sets a address at the given index
-    ///@param _index the index at which the address should be set
-    ///@param _address the address that is written at the index
-    function addAddress(uint _index, address _address) external onlyOwner {
-        list[_index] = _address;
+    ///@param _nodeControlAddress the address that is written at the index
+    function changeAddress(address _nodeControlAddress) external onlyOwner {
+        nodeControlContract = _nodeControlAddress;
     }
 }
