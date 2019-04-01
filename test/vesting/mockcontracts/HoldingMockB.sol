5f7e6723fbf4862bdcfef1875fa56a42e5a98c14
Heiko Burkhardt <heiko@slock.it>
2019-04-01 11:10:11 +0200

    tests imporvement

--- test/vesting/mockcontracts/HoldingMockB.sol
+++ test/vesting/mockcontracts/HoldingMockB.sol
@@ -9,8 +9,8 @@ contract HoldingMockB is Holding {
     function initHoldingData()
         internal 
     {
-        addHolding(0xdD870fA1b7C4700F2BD7f44238821C26f7392148, 50, 2000000000);
-        addHolding(0xdD870fA1b7C4700F2BD7f44238821C26f7392148, 44, 2000000000);
+        addHolding(0xdD870fA1b7C4700F2BD7f44238821C26f7392148, 50 ether, 2000000000);
+        addHolding(0xdD870fA1b7C4700F2BD7f44238821C26f7392148, 44 ether, 2000000000);
     }
 
 
