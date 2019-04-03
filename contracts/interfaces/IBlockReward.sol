8ab918ef9774c8c597fce75d9ae29c27b9ea9e9b
ngyam <ngy.am.zs@gmail.com>
2019-04-03 16:33:34 +0200

    feat(validator-set): enum validator states instead of flags
    - enum states + tests modified
    - new is.. functions added for convenience
    - solidity coverage settings fixed
    - utils formatting adjusted, validator enum codes added
    - useless proxy interface deleted
    - fixed some comment formatting in interfaces

--- contracts/interfaces/IBlockReward.sol
+++ contracts/interfaces/IBlockReward.sol
@@ -3,15 +3,15 @@ pragma solidity ^0.5.0;
 
 /// @title BlockReward contract interface
 /// @dev This interface must be implemented by a system contract that implements the
-/// reward logic.
+/// reward logic
 interface IBlockReward {
 
     /// @notice Produces rewards for the given benefactors,
-    /// with corresponding reward codes.
-    /// @dev Only callable by `SYSTEM_ADDRESS`.
-    /// @param benefactors The list of addresses who can be rewarded.
-    /// @param kind The corresponding list of reward types for the benefactors.
-    /// @return The list of addresses to be rewarded and the corresponding list of reward amounts.
+    /// with corresponding reward codes
+    /// @dev Only callable by `SYSTEM_ADDRESS`
+    /// @param benefactors The list of addresses who can be rewarded
+    /// @param kind The corresponding list of reward types for the benefactors
+    /// @return The list of addresses to be rewarded and the corresponding list of reward amounts
     function reward(address[] calldata benefactors, uint16[] calldata kind)
         external
         returns (address[] memory, uint256[] memory);
--- contracts/interfaces/IValidatorSet.sol
+++ contracts/interfaces/IValidatorSet.sol
@@ -3,29 +3,29 @@ pragma solidity ^0.5.4;
 
 /// @title Reporting validator set interface
 /// @dev This interface is used by Aura PoA validator contracts to manage active validators and
-/// send reports on misbehaving validators.
+/// send reports on misbehaving validators
 interface IValidatorSet {
 
     /// @notice Issue this log event to signal a desired change in validator set.
     /// This will not lead to a change in active validator set until
-    /// finalizeChange is called.
+    /// finalizeChange is called
     /// @dev Only the last log event of any block can take effect.
     /// If a signal is issued while another is being finalized it may never
-    /// take effect.
-    /// @param _parentHash The parent block hash. Otherwise the signal will not be recognized.
-    /// @param _newSet List of addresses of the desired new validator set.
+    /// take effect
+    /// @param _parentHash The parent block hash. Otherwise the signal will not be recognized
+    /// @param _newSet List of addresses of the desired new validator set
     event InitiateChange(bytes32 indexed _parentHash, address[] _newSet);
 
     /// @notice Called when an initiated change reaches finality and is activated.
     /// It is also called when the contract is first enabled for consensus. In this case,
-    /// the "change" finalized is the activation of the initial set.
-    /// @dev Only valid when msg.sender == SYSTEM (EIP96, 2**160 - 2).
+    /// the "change" finalized is the activation of the initial set
+    /// @dev Only valid when msg.sender == SYSTEM (EIP96, 2**160 - 2)
     function finalizeChange()
         external;
 
-    /// @notice Reports benign misbehavior of a validator in the current validator set (e.g. validator offline).
-    /// @param validator Address of the validator to report.
-    /// @param blockNumber The blocknumber to report on.
+    /// @notice Reports benign misbehavior of a validator in the current validator set (e.g. validator offline)
+    /// @param validator Address of the validator to report
+    /// @param blockNumber The blocknumber to report on
     function reportBenign(address validator, uint256 blockNumber)
         external;
 
