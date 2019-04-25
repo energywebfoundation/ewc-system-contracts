4b5b9a2a5a10d659989c8411f12327988e3a0c60
ngyam <ngy.am.zs@gmail.com>
2019-04-25 19:59:28 +0200

    feat(registry): license notice
    apache v2 -> gplv3

--- contracts/registry/Registry.sol
+++ contracts/registry/Registry.sol
@@ -1,18 +1,29 @@
-//! The registry interface.
+//! Copyright (C) 2019 Energy Web Foundation
 //!
-//! Copyright 2016 Gavin Wood, Parity Technologies Ltd.
+//! Licensed under GPL3.
+//! You should have received a copy of the GNU General Public License v 3.0 
+//! along with this file. If not, see 
 //!
-//! Licensed under the Apache License, Version 2.0 (the "License");
-//! you may not use this file except in compliance with the License.
-//! You may obtain a copy of the License at
+//!     https://www.gnu.org/licenses/gpl-3.0.en.html.
 //!
-//!     http://www.apache.org/licenses/LICENSE-2.0
+//! This file incorporates work covered by the following copyright and  
+//! permission notice:
 //!
-//! Unless required by applicable law or agreed to in writing, software
-//! distributed under the License is distributed on an "AS IS" BASIS,
-//! WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//! See the License for the specific language governing permissions and
-//! limitations under the License.
+//!     The registry interface.
+//!
+//!     Copyright 2016 Gavin Wood, Parity Technologies Ltd.
+//!
+//!     Licensed under the Apache License, Version 2.0 (the "License");
+//!     you may not use this file except in compliance with the License.
+//!     You may obtain a copy of the License at
+//!
+//!         http://www.apache.org/licenses/LICENSE-2.0
+//!
+//!     Unless required by applicable law or agreed to in writing, software
+//!     distributed under the License is distributed on an "AS IS" BASIS,
+//!     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//!     See the License for the specific language governing permissions and
+//!     limitations under the License.
 
 pragma solidity 0.5.7;
 
