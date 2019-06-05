274f632fbefc26026f345cc55b2c4e14131cfd13
jbentke <jonas.bentke@slock.it>
2019-06-05 16:03:08 +0200

    51 update confirm check (#68)
    * added require
    
    * added error message
    
    * added test
    
    * Revert "added error message"
    
    This reverts commit 9a22ba6a530a8eae9ed5ee71dac2a133cbb9155d.
    
    * Revert "Revert "added error message""
    
    This reverts commit c71d2fcc5eea59acb265da31b204ec444fcdea53.
    
    * Revert "added test"
    
    This reverts commit c15aa0fd9361169c04dba4c5fbc787c9f6539f51.
    
    * removed testfile formatting

--- contracts/nodecontrol/NodeControlSimple.sol
+++ contracts/nodecontrol/NodeControlSimple.sol
@@ -24,6 +24,7 @@ contract NodeControlSimple is INodeControl, Ownable {
         external 
     {
         require(nodeControlDb.getDockerSha(msg.sender).length != 0, "Error: You are not a validator!");
+        require(!this.isUpdateConfirmed(msg.sender), "Error: Already Confirmed");
         nodeControlDb.setUpdateConfirmed(msg.sender);
     }
 
