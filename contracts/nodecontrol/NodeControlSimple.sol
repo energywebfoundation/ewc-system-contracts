c71d2fcc5eea59acb265da31b204ec444fcdea53
ewf-updater <ewf-updater@ewf.org>
2019-06-05 15:09:33 +0200

    Revert "added error message"
    This reverts commit 9a22ba6a530a8eae9ed5ee71dac2a133cbb9155d.

--- contracts/nodecontrol/NodeControlSimple.sol
+++ contracts/nodecontrol/NodeControlSimple.sol
@@ -24,7 +24,7 @@ contract NodeControlSimple is INodeControl, Ownable {
         external 
     {
         require(nodeControlDb.getDockerSha(msg.sender).length != 0, "Error: You are not a validator!");
-        require(!this.isUpdateConfirmed(msg.sender), "Error: Already Confirmed");
+        require(this.isUpdateConfirmed(msg.sender));
         nodeControlDb.setUpdateConfirmed(msg.sender);
     }
 
